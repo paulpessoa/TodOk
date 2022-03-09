b92c8ba0861ae1a9d0ed7e352fdf768d06b0e3ae
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-09 01:35:37 -0300

    title app style

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's Tasks*/}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Today's Tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go!*/}
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f0",
+  },
+  tasksWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items:{},
+});
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ expo init Todok
 expo start
 
 
+
 Figma Desig - https://www.figma.com/file/ndTNp9FBR5s2KYsJsKPuyg/React-Native-Todo-List?node-id=0%3A1
 
 
