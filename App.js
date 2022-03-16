30d5770905e2755d4b9436d3cddf5f57e8c2536f
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-16 00:16:53 -0300

    styling tasks boxes

--- App.js
+++ App.js
@@ -36,5 +36,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  items:{},
+  items:{
+    marginTop: 30,
+  },
 });
--- components/Task.js
+++ components/Task.js
@@ -14,7 +14,37 @@ const Task = (props) => {
 };
 
 const styles = StyleSheet.create({
-   
+    item: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55bcf6',
+        opacity: 0.4,
+        marginRight: 15,
+    },
+    itemText: {
+        maxWidth: '80%',
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: 'red',
+        borderWidth: 2,
+        borderRadius: 5,
+    }
 });
 
 export default Task;
